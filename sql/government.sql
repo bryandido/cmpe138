95dac17ee68435f16fa0183d3e0d045a7d68e5cd
Clif <cliftonf87@gmail.com>
2018-04-29 12:11:21 -0700

    Updated sql script with cascading options; Removed 'is_elected' table; Updated any references I missed the first time around

--- sql/government.sql
+++ sql/government.sql
@@ -1,12 +1,9 @@
--- 1. drop the database government if it exists
--- 2. create the database government
--- 3. set the current DB context to the newly created database, and then execute the DDL statements.
+-- create the government DB if it does not exist and set it to current DB context
 
 drop database if exists government;
 create database government;
 use government;
 
--- TODO: Decide which tables would require on delete/on update cascade/null options
 CREATE TABLE citizen (
 	ssn				char(9),
     first_name		varchar(15),
@@ -28,25 +25,33 @@ CREATE TABLE immigrant (
     primary key (visa_number)
 );
 
--- is the foreign key setting right?
 CREATE TABLE can_become (
 	ssn			char(9),
     visa_number	char(8),
     primary key (ssn, visa_number),
-    foreign key (ssn) references citizen(ssn) ON DELETE CASCADE,
-    foreign key (visa_number) references immigrant(visa_number) ON DELETE CASCADE
+    foreign key (ssn) references citizen(ssn)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+    foreign key (visa_number) references immigrant(visa_number)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE deported (
-	deporation_id	varchar(8),
+	deporation_id	integer,
     primary key (deporation_id)
 );
 
 CREATE TABLE can_be (
 	visa_number		char(8),
-    deporation_id	varchar(8),
+    deporation_id	integer,
     primary key (visa_number, deporation_id),
+    foreign key (visa_number) references immigrant(visa_number)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
     foreign key (deporation_id) references deported(deporation_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE workforce (
@@ -55,14 +60,20 @@ CREATE TABLE workforce (
     ssn			char(9),
     primary key (tax_id),
     foreign key (ssn) references citizen(ssn)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE joins (
 	visa_number	char(8),
     tax_id		integer,
     primary key (visa_number, tax_id),
-    foreign key (visa_number) references immigrant(visa_number),
+    foreign key (visa_number) references immigrant(visa_number)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
     foreign key (tax_id) references workforce(tax_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE government (
@@ -70,6 +81,8 @@ CREATE TABLE government (
     tax_id			integer,
     primary key (department_id),
     foreign key (tax_id) references workforce (tax_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE justice_department (
@@ -77,22 +90,31 @@ CREATE TABLE justice_department (
     position		varchar(15),
     primary key (department_id),
     foreign key (department_id) references government(department_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
+-- sentence can be something like '10 years' or '6 months'
 CREATE TABLE criminal (
 	prisoner_id	integer,
     sentence	varchar(10),
     ssn			varchar(9),
     primary key (prisoner_id),
     foreign key (ssn) references citizen(ssn)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE imprisons (
 	department_id	varchar(15),
     prisoner_id		integer,
     primary key (department_id, prisoner_id),
-    foreign key (department_id) references justice_department(department_id),
+    foreign key (department_id) references justice_department(department_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
     foreign key (prisoner_id) references criminal(prisoner_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE military_personnel (
@@ -103,6 +125,8 @@ CREATE TABLE military_personnel (
     ssn			varchar(9),
     primary key (military_id),
     foreign key (ssn) references citizen(ssn)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE military_branch (
@@ -110,6 +134,8 @@ CREATE TABLE military_branch (
     military_id	varchar(9),
     primary key (hq_address),
     foreign key (military_id) references military_personnel(military_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE airforce (
@@ -117,6 +143,8 @@ CREATE TABLE airforce (
     plane_type	varchar(20),
     primary key (hq_address),
     foreign key (hq_address) references military_branch(hq_address)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE navy (
@@ -124,6 +152,8 @@ CREATE TABLE navy (
     ship_type	varchar(20),
     primary key (hq_address),
     foreign key (hq_address) references military_branch(hq_address)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE army (
@@ -131,27 +161,29 @@ CREATE TABLE army (
     tank_type	varchar(20),
     primary key (hq_address),
     foreign key (hq_address) references military_branch(hq_address)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE politician (
 	politician_id	varchar(9),
-    primary key (politician_id)
+    ssn			char(9),
+    primary key (politician_id),
+    foreign key (ssn) references citizen(ssn)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE is_elected (
-	ssn				char(9),
-    politician_id	varchar(9),
-    primary key (ssn, politician_id),
-    foreign key (ssn) references citizen(ssn),
-    foreign key (politician_id) references politician(politician_id)
-);
-
-CREATE TABLE vote_for (
+CREATE TABLE votes_for (
 	ssn				char(9),
 	politician_id	varchar(9),
     primary key (ssn, politician_id),
-    foreign key (ssn) references citizen(ssn),
+    foreign key (ssn) references citizen(ssn)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
     foreign key (politician_id) references politician(politician_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 -- term format '2000-2004'
@@ -159,49 +191,73 @@ CREATE TABLE senator (
 	politician_id	varchar(9),
     state			varchar(15),
     term			char(9),		
-    primary key (politician_id)
+    primary key (politician_id),
+    foreign key (politician_id) references politician(politician_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE bill (
-	bill_id	integer,
+	bill_id		varchar(10),
+    description	varchar(100),
     primary key (bill_id)
 );
 
 CREATE TABLE writes (
 	politician_id	varchar(9),
-    bill_id			integer,
+    bill_id			varchar(10),
     primary key (politician_id, bill_id),
-    foreign key (politician_id) references politician(politician_id),
+    foreign key (politician_id) references politician(politician_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
     foreign key (bill_id) references bill(bill_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE vote_on (
+CREATE TABLE votes_on (
 	politician_id	varchar(9),
-    bill_id			integer,
+    bill_id			varchar(10),
     primary key (politician_id, bill_id),
-    foreign key (politician_id) references politician(politician_id),
+    foreign key (politician_id) references politician(politician_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
     foreign key (bill_id) references bill(bill_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
--- do we need something to seperate politician and head_of_state??
 CREATE TABLE head_of_state (
 	politician_id	varchar(9),
     term			char(9),
-    primary key (politician_id)
+    address			varchar(50),
+    primary key (politician_id),
+    foreign key (politician_id) references politician(politician_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	foreign key (address) references military_branch(hq_address)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE law (
 	law_id		varchar(10),
     description	varchar(100),
-    bill_id	integer,
+    bill_id		varchar(10),
     primary key (law_id),
     foreign key (bill_id) references bill(bill_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE follows (
 	department_id	varchar(15),
-    bill_id			integer,
-    primary key (department_id, bill_id),
-    foreign key (department_id) references justice_department(department_id),
-    foreign key (bill_id) references bill(bill_id)
+    law_id			varchar(10),
+    primary key (department_id, law_id),
+    foreign key (department_id) references justice_department(department_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+    foreign key (law_id) references law(law_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
