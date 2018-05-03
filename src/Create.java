b2a622315d4510965530482c1c0e8021e159e781
bryandido <balanyan.kyaw@sjsu.edu>
2018-05-03 00:00:51 -0700

    create 20% done

--- src/Create.java
+++ src/Create.java
@@ -17,28 +17,45 @@ public class Create {
 		if (c == 'p') { // if politician
 			System.out.println("Registation for Politician");
 			RegisterPolitician();
-			System.out.println("Now login...");
+			System.out.println("Now to login...");
 		} 
 		else if (c == 'm') { // if military personnel
 			System.out.println("Registation for Military Personnel");
 			RegisterMilitaryPersonnel();
-			System.out.println("Now login...");
+			System.out.println("Now to login...");
 		} 
 		else { // if civilian
 			System.out.println("Registation for Civilian");
 			RegisterCivilian();
-			System.out.println("Now login...");
+			System.out.println("Now to login...");
 		}
 	}
 	public void RegisterPolitician() {
+		// create civilian first
 		RegisterCivilian();
+		//query
 		
 	}
 	public void RegisterMilitaryPersonnel() {
+		// create civilian first
 		RegisterCivilian();
 		
 	}
 	public void RegisterCivilian() {
 		System.out.println("Enter your SSN: ");
+		SSN = sc.next();
+		System.out.println("Enter your password: ");
+		password = sc.next();
+		System.out.println("Enter your first name: ");
+		firstName = sc.next();
+		System.out.println("Enter your middle initial: ");
+		middleInitial = sc.next();
+		System.out.println("Enter your last name: ");
+		lastName = sc.next();
+		System.out.println("Enter your birthDate");
+		birthDate = sc.next();
+		System.out.println("Enter address: ");
+		address = sc.next();
+		//query
 	}
 }
